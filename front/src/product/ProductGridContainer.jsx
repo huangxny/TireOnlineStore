e7abfd512308b5947b5956635b18b7b9f2ca01a6
Eduardo Puerta <60192270+epuer18@users.noreply.github.com>
2023-12-07 16:16:17 -0500

    Add more columns in  ProductGridContainer.jsx

--- front/src/product/ProductGridContainer.jsx
+++ front/src/product/ProductGridContainer.jsx
@@ -2,6 +2,8 @@ import AddToCart from './AddToCart.jsx';
 import PropTypes from "prop-types";
 const ProductGridContainer = ({ products, removeProduct, email, cart, setCart }) => {
   return (
+    // Code Reviewer: I would recommend using bootstrap and the grid to make at least 2 columns
+    // the list is hard to read in a single column
     <ul className='gridContainer'>
       {products.map((product) => (
         <li className='item' key={product._id}>
